4e631cf7b27ed77422db65214cdc915a1166604b
mbatshi826 <mbatshi826@gmail.com>
2025-11-25 17:05:31 +0000

    Fix table name in CustomerDAO

--- src/main/java/dao/CustomerDAO.java
+++ src/main/java/dao/CustomerDAO.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class CustomerDAO {
 
     public Customer create(Customer c) throws SQLException {
-        String sql = "INSERT INTO customers(first_name,last_name,address) VALUES(?,?,?)";
+        String sql = "INSERT INTO customer(first_name,last_name,address) VALUES(?,?,?)";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, c.getFirstName());
@@ -25,7 +25,7 @@ public class CustomerDAO {
     }
 
     public Customer findById(long id) throws SQLException {
-        String sql = "SELECT * FROM customers WHERE id = ?";
+        String sql = "SELECT * FROM customer WHERE id = ?";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setLong(1, id);
@@ -41,7 +41,7 @@ public class CustomerDAO {
 
     public List<Customer> findAll() throws SQLException {
         List<Customer> list = new ArrayList<>();
-        String sql = "SELECT * FROM customers";
+        String sql = "SELECT * FROM customer";
         try (Connection conn = DBConnection.getConnection();
              Statement st = conn.createStatement();
              ResultSet rs = st.executeQuery(sql)) {
@@ -55,7 +55,7 @@ public class CustomerDAO {
     }
 
     public boolean update(Customer c) throws SQLException {
-        String sql = "UPDATE customers SET first_name=?, last_name=?, address=? WHERE id=?";
+        String sql = "UPDATE customer SET first_name=?, last_name=?, address=? WHERE id=?";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, c.getFirstName());
@@ -67,7 +67,7 @@ public class CustomerDAO {
     }
 
     public boolean delete(long id) throws SQLException {
-        String sql = "DELETE FROM customers WHERE id=?";
+        String sql = "DELETE FROM customer WHERE id=?";
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setLong(1, id);
